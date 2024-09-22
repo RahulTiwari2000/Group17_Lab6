d70c5ecc3ea26670da6ec6a95f396fb74fa5295f
Shivenraghav2001 <EE24MT030@iitdh.ac.in>
2024-09-22 16:36:11 +0530

    Created some Changes in Configuring pin and interrupt

--- G17_LAB06A/main.c
+++ G17_LAB06A/main.c
@@ -13,20 +13,22 @@ int main(void) {
 
 }
 void init(void) {
-
     SYSCTL_RCGCGPIO_R |= 0x020;
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R = 0x10;
-    GPIO_PORTF_DIR_R = 0x02;
-    GPIO_PORTF_DEN_R = 0x12; // Digital enable for PF1 and PF4
-    GPIO_PORTF_PUR_R |= 0x01;  // Enable pull-up resistor for the switch
+                       GPIO_PORTF_LOCK_R = 0x4C4F434B;
+                       GPIO_PORTF_CR_R |= 0x10;          // Allow changes to PF4
 
-    GPIO_PORTF_IS_R = 0x00;   // Edge-sensitive
-    GPIO_PORTF_IBE_R = 0x00;  // Interrupt controlled by IEV
-    GPIO_PORTF_IEV_R = 0x00;  // Falling edge trigger (button press)
-    GPIO_PORTF_ICR_R = 0x10;   // Clear any prior interrupt
-    GPIO_PORTF_IM_R = 0x10;    // Unmask interrupt for PF4
+                           // Configure PF1 as output (LED)
+                           GPIO_PORTF_DIR_R = 0x02;
+                           GPIO_PORTF_DEN_R = 0x13;
 
-    NVIC_EN0_R = 0x40000000 ;
+                           GPIO_PORTF_PUR_R = 0x11;
 
+
+                       GPIO_PORTF_IS_R = 0x00;   //  edge-sensitive
+                          GPIO_PORTF_IBE_R = 0x00;  // Interrupt controlled by IEV
+                          GPIO_PORTF_IEV_R = 0x00;  // Falling edge trigger
+                          GPIO_PORTF_ICR_R = 0x11;   // removing prior interrupt
+                          GPIO_PORTF_IM_R = 0x11;    // Unmask interrupt for PF4
+
+                          NVIC_EN0_R = 0x40000000;
 }
